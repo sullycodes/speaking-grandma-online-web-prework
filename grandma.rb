3ff68b30f0f8eecacb886a68ac9a199c23e533e0
Daniel Sullivan <dansullivan19@gmail.com>
2019-03-15 21:28:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
+  if phrase == phrase.upcase && !"I LOVE YOU GRANDMA!"
+    puts 
   
 
 # Whatever you say to grandma, she should respond with
